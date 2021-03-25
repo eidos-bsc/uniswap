011ce8cb3ade26bb8e6d58651e78182075478de5
eidos-bsc <yilun.zhao22@gmail.com>
2021-03-26 01:12:10 +0800

    first commit

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,7 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 
 import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 
-export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
+export const ROUTER_ADDRESS = '0x11f24689B00a2d4a3A09A751cfC8e81a3c938A6e'
 
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
